cd8058ede8a5f69b474fe1cb5af28c3f22817584
hhvrc <github@heavenvr.tech>
2024-09-11 22:55:45 +0200

    Assign weights to words

--- DiscordBot/MessageHandler/MessageHandler.cs
+++ DiscordBot/MessageHandler/MessageHandler.cs
@@ -5,45 +5,43 @@ namespace OpenShock.DiscordBot.MessageHandler;
 
 public sealed class MessageHandler
 {
-    private static readonly HashSet<string> _containedProfanities =
-    [
-        "fuck",
-        "pishock",
-        "arduino",
-        "penis",
-        "pussy",
-        "damnit",
-        "cocksucker",
-        "retard",
-        "dick",
-        "bitch",
-        "bastard",
-        "cunt",
-        "slut",
-        "shit"
-    ];
+    private static readonly Dictionary<string, float> _containedProfanities = new() {
+        {"fuck", 1f },
+        {"pishock", 1f },
+        {"arduino", 1f },
+        {"penis", 1f },
+        {"pussy", 1f },
+        {"damnit", 1f },
+        {"cocksucker", 1f },
+        {"retard", 1f },
+        {"dick", 1f },
+        {"bitch", 1f },
+        {"bastard", 1f },
+        {"cunt", 1f },
+        {"slut", 1f },
+        { "shit", 1f },
+    };
 
-    private static readonly HashSet<string> _standaloneProfanities =
-    [
-        "ass",
-        "cock",
-        "damn",
-        "crap",
-        "piss",
-        "anal",
-        "cum"
-    ];
+    private static readonly Dictionary<string, float> _standaloneProfanities = new() {
+        {"ass", 1f },
+        {"cock", 1f },
+        {"damn", 1f },
+        {"crap", 1f },
+        {"piss", 1f },
+        {"anal", 1f },
+        { "cum", 1f },
+    };
 
     private static bool ContainsProfanities(string str)
     {
-        if (_containedProfanities.Any(str.Contains))
+        if (_containedProfanities.Keys.Any(str.Contains))
         {
             return true;
         }
 
         var lcWordsSet = str.ToLower().Split([' ', '\t', '\r', '\n'], StringSplitOptions.RemoveEmptyEntries);
 
-        if (_standaloneProfanities.Overlaps(lcWordsSet))
+        if (lcWordsSet.Any(_standaloneProfanities.ContainsKey))
         {
             return true;
         }
